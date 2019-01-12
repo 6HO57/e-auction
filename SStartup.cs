08a7f6c05fbdd362955b5cd5cad0e270e5a6257d
6HO57 <34879818+6HO57@users.noreply.github.com>
2019-01-12 12:54:52 -0800

    Add files via upload

---
+++ SStartup.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_auction.SStartup))]
+
+namespace e_auction
+{
+    public partial class SStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Configuration(app);    
+            app.MapSignalR();
+        }
+    }
+}
